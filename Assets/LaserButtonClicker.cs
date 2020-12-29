d0cba16d4a36c4e58053b93723421c39a7e24493
root <root@LAPTOP-K4Q9C0IQ.localdomain>
2020-12-29 06:41:30 -0500

    upload

---
+++ Assets/LaserButtonClicker.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.Extras;
+using Valve.VR.InteractionSystem;
+using UnityEngine.UI;
+using Valve.VR;
+
+
+
+public class LaserButtonClicker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SteamVR_Input_Sources myHand;
+    public SteamVR_Action_Boolean interactAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("default","InteractUI");
+    private SteamVR_LaserPointer laserPointer;
+    private GameObject btn;
+
+    private bool pointerOnButton = false;
+    
+    void Start()
+    {
+        laserPointer = GetComponent<SteamVR_LaserPointer>();
+        laserPointer.PointerIn += LaserPointer_PointerIn;
+        laserPointer.PointerOut += LaserPointer_PointerOut;
+        
+    }
+
+    private void LaserPointer_PointerIn(object sender, PointerEventArgs e)
+    {
+        if (e.target.gameObject.GetComponent<Button>() != null && btn ==null)
+        {
+            btn = e.target.gameObject;
+            InputModule.instance.HoverBegin(btn);
+            pointerOnButton = true;
+        }
+    }
+
+    private void LaserPointer_PointerOut(object sender, PointerEventArgs e)
+    {
+        if (assertNotNull(btn))
+        {
+            pointerOnButton = false;
+            InputModule.instance.HoverEnd(btn);
+            btn = null;
+        }
+    }
+    
+    void Update()
+    {
+        if (pointerOnButton)
+        {
+            if (interactAction[myHand].stateDown)
+            {
+                InputModule.instance.Submit(btn);
+            }
+        }
+    }
+}
---
+++ Assets/MenuController.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+using Valve.VR;
+
+
+public class MenuController : MonoBehaviour
+{
+    public SteamVR_LoadLevel _levelLoader;
+
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        Debug.Log("Starting Game");
+        _levelLoader.levelName = "main_scene";
+        _levelLoader.fadeOutTime = 1f;
+        _levelLoader.Trigger();
+    }
+
+    // Update is called once per frame
+    public void ExitGame()
+    {
+        Debug.Log("Exiting Game");
+        SteamVR_Fade.Start(Color.black, 2);
+    }
+}
